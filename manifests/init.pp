8d215d3bca1d6fe21e1b92ee01a0b270d093968b
Jon Pascoe <jon.pascoe@me.com>
2013-04-02 09:28:33 +0100

    Added repository

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# Public: Install and configure dotfiles in your home directory
+#
+# Examples
+#
+#   include dotfiles
+class dotfiles {
+  repository { "${boxen::config::srcdir}/dotfiles"
+    source => 'pacso/dotfiles
+  }
+}
